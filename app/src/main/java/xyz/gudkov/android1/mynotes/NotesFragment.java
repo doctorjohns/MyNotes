2932cc9aaa15b7351117616f952ce5192c4994f9
GudkovDmitry <gudkovdm@gmail.com>
2021-03-18 16:11:21 +0300

    add landscape orientation

--- app/src/main/java/xyz/gudkov/android1/mynotes/NotesFragment.java
+++ app/src/main/java/xyz/gudkov/android1/mynotes/NotesFragment.java
@@ -1,9 +1,12 @@
 package xyz.gudkov.android1.mynotes;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -16,28 +19,75 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class NotesFragment extends Fragment {
+
+    public static final String CURRENT_NOTE = "CurrentNote";
+    private int currentPosition = 0;
+    private boolean isLandscape;
+
     @Override
-    public View onCreateView (LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_notes, container, false);
     }
+
     @Override
-    public void onViewCreated (@NotNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view,savedInstanceState);
+    public void onViewCreated(@NotNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
         initList(view);
     }
 
     private void initList(View view) {
-        LinearLayout layoutView = (LinearLayout)view;
+        LinearLayout layoutView = (LinearLayout) view;
         String[] notes = getResources().getStringArray(R.array.notes);
-        for (int i = 0; i < notes.length ; i++) {
+        for (int i = 0; i < notes.length; i++) {
             String note = notes[i];
             TextView tv = new TextView(getContext());
             tv.setText(note);
             tv.setTextSize(30);
             layoutView.addView(tv);
-            final  int fi = i;
-            tv.setOnClickListener(v -> showNoteContent(fi));
+            final int fi = i;
+            tv.setOnClickListener(v -> {
+                currentPosition = fi;
+                showNote(currentPosition);
+            });
         }
+    }
+    @Override
+    public void onSaveInstanceState (@NotNull Bundle outstate) {
+        outstate.putInt(CURRENT_NOTE, currentPosition);
+        super.onSaveInstanceState(outstate);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        isLandscape = getResources().getConfiguration().orientation
+                == Configuration.ORIENTATION_LANDSCAPE;
+
+        if (savedInstanceState != null) {
+            currentPosition = savedInstanceState.getInt(CURRENT_NOTE,0);
+        }
+
+        if (isLandscape) {
+            showLandNoteContent(0);
+        }
+    }
+
+    private void showNote(int index) {
+        if (isLandscape) {
+            showLandNoteContent(index);
+        } else {
+            showNoteContent(index);
+        }
+    }
+
+    private void showLandNoteContent(int index) {
+        NoteContentFragment detail = NoteContentFragment.newInstance(index);
+
+        FragmentManager fragmentManager = requireActivity().getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.note_content, detail);
+        fragmentTransaction.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
+        fragmentTransaction.commit();
     }
 
     private void showNoteContent(int index) {
