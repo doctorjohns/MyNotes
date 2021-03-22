a971536ec3699784119bfdc3118ed9d011cfb73e
GudkovDmitry <gudkovdm@gmail.com>
2021-03-23 02:06:43 +0300

    add toolbar

--- app/src/main/java/xyz/gudkov/android1/mynotes/MainActivity.java
+++ app/src/main/java/xyz/gudkov/android1/mynotes/MainActivity.java
@@ -1,8 +1,14 @@
 package xyz.gudkov.android1.mynotes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.toolbar_menu_item1: {
+                Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+            case R.id.toolbar_menu_item2: {
+                Toast.makeText(this,"О программе",Toast.LENGTH_SHORT).show();
+            }
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
